3d48fb9df6e837b4bc12897fd9ca7852d738ef2c
donghwi-kor <donghwi.kor@gmail.com>
2021-03-15 09:58:32 +0900

    app.js 수정

--- js/app.js
+++ js/app.js
@@ -13,7 +13,6 @@ const modalClose2 = document.querySelector(".close2");
 const modalClose3 = document.querySelector(".close3");
 
 
-// 조건문으로 가능한지 확인
 modalBtn1.addEventListener("click", function(){
     modalBg1.classList.add("bg-active");
 });
@@ -38,33 +37,3 @@ modalClose3.addEventListener("click", function(){
     modalBg3.classList.remove("bg-active");
 });
 
-
-// 서브바 클릭시 분류
-// const subBar1 = document.querySelector(".sub-hulk");
-// const a;
-
-
-// subBar1.addEventListener("click", function() {
-//     if(a==1)
-//         {
-//             document.getElementById("item-list-container").display="inline";
-//             return a=0;    
-//         }
-//     else
-//     {
-//         document.getElementById("item-list-container").display="none";
-//         return a=1;
-//     }
-// })
-
-// const ball = document.querySelector(".sub-hulk");
-// const items = document.querySelectorAll(
-//   ".item-list-container"
-// );
-
-// ball.addEventListener("click", () => {
-//   items.forEach((item) => {
-//     item.classList.toggle("active");
-//   });
-//   ball.classList.toggle("active");
-// });
